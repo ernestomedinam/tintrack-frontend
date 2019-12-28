f982f99a5d7d1e6a21ba53ed138052bf0ff90f9f
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2019-12-27 21:52:53 -0400

    routine view completed, move on to 002-b.

--- src/js/views/Routine.js
+++ src/js/views/Routine.js
@@ -4,6 +4,7 @@ import "../../sass/views/Routine.scss";
 import EditTrack from "../components/EditTrack";
 import { AppContext } from "../store/AppContext.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Badge } from "react-bootstrap";
 
 const Routine = prop => {
 	const { store, actions } = useContext(AppContext);
@@ -12,7 +13,27 @@ const Routine = prop => {
 			<Container className="routine-wrapper">
 				<Container className="routine-tools bg-dark">
 					<div className="title">
-						<h4>{"Routine items"}</h4>
+						<h4>{"routine items"}</h4>
+					</div>
+					<div className="tasks text-center">
+						<p className="m-0">
+							<Badge pill variant="primary" className="mr-2">
+								{store.routine.plannedTasks.length}
+							</Badge>
+							{"tasks"}
+						</p>
+					</div>
+					<div className="habits text-center">
+						<p className="m-0">
+							{"habits"}
+							<Badge pill variant="primary" className="ml-2">
+								{store.routine.habitCounters.length}
+							</Badge>
+						</p>
+					</div>
+					<div className="add-habit text-center">
+						<FontAwesomeIcon icon={["far", "plus-square"]} />
+						<span className="legend">{"add habit"}</span>
 					</div>
 					<div className="add-task text-center">
 						<span className="legend">{"add task"}</span>
