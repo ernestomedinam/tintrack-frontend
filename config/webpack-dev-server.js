f3f4c8cd95eea7518831c210c6ec7f9c744c1346
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-02-12 22:37:16 +0000

    gitpod working, changed 0.0.0.0 to localhost, disableHostCheck=true removed.

--- config/webpack-dev-server.js
+++ config/webpack-dev-server.js
@@ -4,7 +4,7 @@ const paths = require("./paths.js");
 const config = require("./webpack-dev-config.js");
 
 const port = 3000;
-const host = "0.0.0.0";
+const host = "localhost";
 
 const options = {
     host: host,
@@ -13,7 +13,6 @@ const options = {
     open: true,
     contentBase: [paths.appAssets, paths.appPublic],
     watchContentBase: true,
-    disableHostCheck: true,
     historyApiFallback: true,
     after() {
         console.log(
